e6d3d66f89b5a919cec2ae66cf4d89d27713c755
Marshall Brown <mdbrown@fhcrc.org>
2016-01-11 22:39:04 -0800

    fix a typo in description

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Simulated dataset for package 'DecisionCurve'
 #'
-#' A simulated dataset containing demographic variables,
+#' Simulated cohort data containing demographic variables,
 #' marker values and cancer outcome.
 #'
 #' @format A data frame with 500 rows and 6 variables:
@@ -13,3 +13,21 @@
 #'   \item Cancer: Indicator for cancer.
 #' }
 "dcaData"
+
+
+#' Simulated dataset for package 'DecisionCurve'
+#'
+#' Simulated data from a case-control study containing demographic variables,
+#' marker values and cancer outcome. The prevalence of cancer
+#' in the population that this data is sampled from is approximately 0.11.
+#'
+#' @format A data frame with 500 rows and 6 variables:
+#' \itemize{
+#'   \item Age: Age in years.
+#'   \item Female: Indicator for female gender.
+#'   \item Smokes: Indicator for smoking status.
+#'   \item Marker1: simulated biomarker.
+#'   \item Marker2: simulated biomarker.
+#'   \item Cancer: Indicator for cancer.
+#' }
+"dcaData_cc"
